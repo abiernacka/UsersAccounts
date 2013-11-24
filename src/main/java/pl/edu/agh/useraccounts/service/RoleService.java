8b2f5b1ab7a1934ac9b3152ce3f2a1600367cf76
azelezik <agnieszkazelezik@gmail.com>
2013-11-24 20:07:01 +0100

    Komentarze dla metod dotyczących ról użytkownika

--- src/main/java/pl/edu/agh/useraccounts/service/RoleService.java
+++ src/main/java/pl/edu/agh/useraccounts/service/RoleService.java
@@ -7,15 +7,55 @@ import java.util.List;
 @WebService
 public interface RoleService {
 
+    /**
+     * Utworzenie nowej roli w systemie.
+     * W przypadku powodzenia zwraca 0, 1 w przypadku niepowodzenia (podana rola już istanieje).
+     * @param role Nazwa nowej roli
+     * @return Rezultat (zero lub kod błędu)
+     */
     public int createRole(String role);
 
+    /**
+     * Usunięcie roli z systemu. Jednocześnie użytkonicy, którzy mieli przypisaną daną rolę tracą ją.
+     * W przypadku powodzenia zwraca 0, 1 w przypadku niepowodzenia (podana rola nie istanieje).
+     * @param role Nazwa roli do usunięcia
+     * @return Rezultat (zero lub kod błędu)
+     */
     public int removeRole(String role);
 
+    /**
+     * Dodanie użytkownikowi danej roli (jeśli użytkownik miał już nadaną podaną rolę zwracane jest 0).
+     * W przypadku powodzenia zwraca 0, w przypadku niepowodzenia:
+     * 1 gdy nie istanie podany login
+     * 2 jeśli login poprawny ale nie istanie podana rola.
+     * @param login Login użytkownika, któremu ma zostać nadana rola
+     * @param role Rola (nazwa roli) jaka ma być nadana użytkownikowi
+     * @return Rezultat (zero lub kod błędu)
+     */
     public int addRole(String login, String role);
 
+    /**
+     * Odbieranie użytkownikowi danej roli.
+     * W przypadku powodzenia zwraca 0, w przypadku niepowodzenia:
+     * 1 gdy nie istanie podany login
+     * 2 jeśli login poprawny ale nie istanie podana rola
+     * 3 jeśli login i rola istanieje, ale nie jest przypisana danemu użytkownikowi.
+     * @param login Login użytkownika, któremu ma zostać nadana rola
+     * @param role Rola (nazwa roli) jaka ma być nadana użytkownikowi
+     * @return Rezultat (zero lub kod błędu)
+     */
     public int revokeRole(String login, String role);
 
+    /**
+     * Pobieranie listy ról użytkownika.Jeśli dany login nie istanie lub użytkownik nie ma przypisanych żadnych ról zwracana jest pusta lista.
+     * @param login Login użytkownika
+     * @return Rezultat (zero lub kod błędu)
+     */
     public List<String> getUserRoles(String login);
 
+    /**
+     * Pobranie listy wszystkich ról w systemie.
+     * @return Lista wszystkich ról w systemie
+     */
     public List<String> getAllRoles();
 }
